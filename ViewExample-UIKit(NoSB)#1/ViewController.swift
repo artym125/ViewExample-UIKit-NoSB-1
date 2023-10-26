e10214a8ef0b92f20b9e2f9a4a4ce1ffa1513208
artym125 <otapik125@gmail.com>
2023-10-26 23:08:32 +0300

    Initial Commit

---
+++ ViewExample-UIKit(NoSB)#1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ViewExample-UIKit(NoSB)#1
+//
+//  Created by Ostap Artym on 26.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
